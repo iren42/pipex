54e47aa3005347dec675a8b0d9cb933e6f28946b
Isabelle REN <iren@e2r1p3.clusters.42paris.fr>
2022-05-05 20:23:15 +0200

    did: protect malloc

--- src/utils.c
+++ src/utils.c
@@ -6,17 +6,11 @@
 /*   By: iren <iren@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 18:17:42 by iren              #+#    #+#             */
-/*   Updated: 2022/03/11 16:54:22 by iren             ###   ########.fr       */
+/*   Updated: 2022/05/05 20:15:20 by iren             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-void	piperror(const char *s, int errcode)
-{
-	perror(s);
-	exit(errcode);
-}
 
 static char	*find_path(char **env)
 {
@@ -32,9 +26,8 @@ static char	*find_path(char **env)
 	return (0);
 }
 
-static void	init_get_cmd(char **tmp, char **res, char ***splitpaths, int *i)
+static void	init_get_cmd(char **res, char ***splitpaths, int *i)
 {
-	*tmp = 0;
 	*res = 0;
 	*splitpaths = 0;
 	*i = 0;
@@ -50,25 +43,48 @@ static char	**get_splitpaths(char **env)
 	if (path == 0)
 		return (0);
 	res = ft_split(path, ':');
+	if (!res)
+		return (0);
+	return (res);
+}
+
+static char	*join(int i, char **splitpaths, char *cmd)
+{
+	char	*tmp;
+	char	*res;
+
+	res = 0;
+	tmp = ft_strjoin(splitpaths[i], "/");
+	if (!tmp)
+	{
+		free_split(splitpaths);
+		return (0);
+	}
+	res = ft_strjoin(tmp, cmd);
+	free(tmp);
+	if (!res)
+	{
+		free_split(splitpaths);
+		return (0);
+	}
 	return (res);
 }
 
 char	*get_cmd(char **env, char *cmd)
 {
-	char	*tmp;
 	char	*res;
 	char	**splitpaths;
 	int		i;
 
-	init_get_cmd(&tmp, &res, &splitpaths, &i);
+	init_get_cmd(&res, &splitpaths, &i);
 	splitpaths = get_splitpaths(env);
 	if (splitpaths != 0)
 	{
 		while (splitpaths[i])
 		{
-			tmp = ft_strjoin(splitpaths[i], "/");
-			res = ft_strjoin(tmp, cmd);
-			free(tmp);
+			res = join(i, splitpaths, cmd);
+			if (!res)
+				return (0);
 			if (access(res, X_OK) == 0)
 			{
 				free_split(splitpaths);
